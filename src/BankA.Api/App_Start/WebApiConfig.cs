684ef29eae6c1e427bafa280fbe7e3254b808dee
figueiredorui <figueiredo.rui@gmail.com>
2015-02-11 22:08:09 +0000

    EnableCors

--- src/BankA.Api/App_Start/WebApiConfig.cs
+++ src/BankA.Api/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace BankA.Api
 {
@@ -16,6 +17,8 @@ namespace BankA.Api
             // Configure Web API to use only bearer token authentication.
             //config.SuppressDefaultHostAuthentication();
             //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
