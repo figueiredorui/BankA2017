b3592df6433675a48e0c45890a0078e5f2baca43
figueiredorui <figueiredo.rui@gmail.com>
2015-03-02 22:03:26 +0000

    Added build task

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -2,6 +2,8 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -10,7 +12,30 @@ module.exports = function(grunt) {
             target: {
                 src: 'index.html' // point to your HTML file.
             }
-        }
+        },
+        copy: {
+            build: {
+                files: [
+                    {cwd: '../src', src: [ 'app/**' ], dest: '../dist', expand: true},
+                    {cwd: '../src', src: [ 'css/**' ], dest: '../dist', expand: true},
+                    {cwd: '../src', src: [ 'img/**' ], dest: '../dist', expand: true},
+                    {cwd: '../src', src: [ 'vendor/**' ], dest: '../dist', expand: true},
+                    {cwd: '../src', src: [ 'index.html' ], dest: '../dist', expand: true},
+                ],
+
+            },
+        },
+        clean: {
+            build: {
+                src: [ '../dist/**' ]
+            },
+        },
     });
+
+    grunt.registerTask(
+  'build',
+  'Compiles all of the assets and copies the files to the dist directory.',
+  [ 'clean', 'copy' ]
+);
 
 };
