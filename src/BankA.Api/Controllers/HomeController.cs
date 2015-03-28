3d0d56f3bd37ae2a34ae2240c803b06532b23a10
figueiredorui <figueiredo.rui@gmail.com>
2015-03-28 21:39:25 +0000

    Version incrementation close #6

--- src/BankA.Api/Controllers/HomeController.cs
+++ src/BankA.Api/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,7 +12,22 @@ namespace BankA.Api.Controllers
     {
         public IHttpActionResult Get()
         {
-            return Ok("Welcome to BankA API");
+            return Ok(new
+            {
+                Name = "BankA API",
+                Version = GetVersion(),
+                GitHub = "https://github.com/figueiredorui/BankA.Api"
+            });
+        }
+
+        private string GetVersion()
+        {
+            Assembly web = Assembly.GetExecutingAssembly();
+            AssemblyName webName = web.GetName();
+
+            string myVersion = webName.Version.ToString();
+
+            return myVersion;
         }
     }
 
