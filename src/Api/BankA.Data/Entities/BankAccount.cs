14227e779ca660ccb0c656591140f91720241f6c
Rui Figueiredo <figueiredo.rui@gmail.com>
2017-05-04 15:50:41 +0100

    make it simple

---
+++ src/Api/BankA.Data/Entities/BankAccount.cs
@@ -0,0 +1,45 @@
+namespace BankA.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("BankAccount")]
+    public partial class BankAccount
+    {
+        public BankAccount()
+        {
+            BankTransactions = new HashSet<BankTransaction>();
+        }
+
+        [Key]
+        public int AccountID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string BankName { get; set; }
+
+        public bool IsSavingsAccount { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        [StringLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? ChangedOn { get; set; }
+
+        [StringLength(50)]
+        public string ChangedBy { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BankTransaction> BankTransactions { get; set; }
+
+        public virtual ICollection<BankFile> BankStatementFiles { get; set; }
+    }
+}
--- src/build.bat
+++ src/build.bat
@@ -8,20 +8,3 @@ CALL bower install
 echo installing grunt packages...
 CALL grunt build
 
-cd..
-
-set PROJ=Desktop\BankA.Wpf\BankA.Wpf.csproj
-
-.nuget\nuget restore BankA.sln
-
-SET MSBUILDEXEDIR=C:\Program Files (x86)\MSBuild\14.0\Bin
-SET MSBUILDEXE=%MSBUILDEXEDIR%\MSBuild.exe
-SET MSBUILDOPT=/verbosity:minimal
-SET CONFIGURATION=Debug
-SET SolutionPath=%~dp0
-SET ReleasePath=%SolutionPath%\Desktop\Release
-
-"%MSBUILDEXE%" /p:Configuration=%CONFIGURATION%;Platform=x86;SolutionDir=%SolutionPath%;OutputPath=%ReleasePath% "%PROJ%"
-
-cd %ReleasePath%
-CALL BankA.Wpf.exe
